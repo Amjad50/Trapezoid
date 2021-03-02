6c50ab028ef29eef9b576a7e0264391a968ab8b2
Amjad Alsharafi <amjadsharafi10@gmail.com>
2021-03-02 12:00:31 +0300

    Front: added ability to specify the number of instructions to run
    We are now just trying to implement the CPU instructions

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,16 @@ use psx_core::Psx;
 fn main() {
     let args: Vec<_> = args().collect();
 
-    if args.len() < 2 {
-        println!("USAGE: {} <bios>", args[0]);
+    if args.len() < 3 {
+        println!("USAGE: {} <bios> <instructions to execute>", args[0]);
         exit(1);
     }
 
+    let count = args[2].parse::<u32>().unwrap();
+
     let mut psx = Psx::new(&args[1]).unwrap();
 
-    for _ in 0..20 {
+    for _ in 0..count {
         psx.clock();
     }
 }
