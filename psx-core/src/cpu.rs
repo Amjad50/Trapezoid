Amjad50/Trapezoid

commit f64d16ae3f843bc2031834da9de8163065406eec

Cpu: logged the current CPU instruction before incrementing
When we removed the `pc` variable, the trace print got affected, and
would print a PC value that is not correct. Fixed now.

psx-core/src/cpu.rs
@@ -58,14 +58,14 @@ impl Cpu {
             if let Some(instruction) = self.bus_read_u32(bus, self.regs.pc) {
                 let instruction = Instruction::from_u32(instruction);
 
-                self.regs.pc += 4;
-
                 if let Some(jump_dest) = self.jump_dest_next.take() {
                     log::trace!("pc jump {:08X}", jump_dest);
                     self.regs.pc = jump_dest;
                 }
 
                 log::trace!("{:08X}: {:02X?}", self.regs.pc, instruction);
+
+                self.regs.pc += 4;
                 self.execute_instruction(&instruction, bus);
 
                 // exit so that we can run dma

commit d9ba8986cf533daa13a7775ad92e99bb14af856d

Cpu: fix bug in incrementing PC register after jump
This was introduced by f64d16a.

psx-core/src/cpu.rs
@@ -58,14 +58,13 @@ impl Cpu {
             if let Some(instruction) = self.bus_read_u32(bus, self.regs.pc) {
                 let instruction = Instruction::from_u32(instruction);
 
+                log::trace!("{:08X}: {:02X?}", self.regs.pc, instruction);
+                self.regs.pc += 4;
                 if let Some(jump_dest) = self.jump_dest_next.take() {
                     log::trace!("pc jump {:08X}", jump_dest);
                     self.regs.pc = jump_dest;
                 }
 
-                log::trace!("{:08X}: {:02X?}", self.regs.pc, instruction);
-
-                self.regs.pc += 4;
                 self.execute_instruction(&instruction, bus);
 
                 // exit so that we can run dma