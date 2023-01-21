a62de0936414bcd179ef495d1cada5500dd9e836
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2023-01-22 00:43:38 +0800

    Gpu: fixed jagged clut textures because of wrong `x` value used as index
    The value used as index is already reduced (divided by `divider`), and
    thus provide very jagged results

--- psx-core/src/gpu/shaders/fragment.glsl
+++ psx-core/src/gpu/shaders/fragment.glsl
@@ -121,7 +121,7 @@ void main() {
             uint color_u16 = u16_from_color_with_alpha(color_value);
 
             uint mask = 0xFFFFu >> (16u - (16u / divider));
-            uint clut_index_shift = (uint(x) % divider) * (16u / divider);
+            uint clut_index_shift = (uint(norm_coord.x) % divider) * (16u / divider);
             uint clut_index = (color_u16 >> clut_index_shift) & mask;
 
             color_value = fetch_color_from_texture(v_clut_base + uvec2(clut_index, 0));
